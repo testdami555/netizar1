ad7fa40f2d810efac4af0d09a866c6abb27fc474
testdami555 <testdami555@gmail.com>
2021-12-22 10:35:51 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+const titulo = "Soy el título del Mundo";
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>{titulo}</h1>
+      <p>
+        Edita <code>src/App.js</code> and save to reload.
+      </p>
+    </header>
+  </div>
+);
+
+export default App;
